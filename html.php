5f309fc70cda8c3bc68513196305cf0d3b6f4bda
Riya0827 <160325583+Riya0827@users.noreply.github.com>
2024-03-02 13:33:45 +0530

    first commit

---
+++ html.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Taxi Services</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    
+</head>
+<body>
+    <form id="vechicleForm">
+        <label for="vehicleName">vehicle Name:</label><br>
+        <input type="text" id="vehicleName" name="vehicleName"><br>
+        <label for="vehicleNumber">vehicle vehicleNumber:</label><br>
+        <input type="text" id="vehicleNumber" name="vehicleNumber"><br>
+        <label for="dieselCost">Diesel Cost Per Trip:</label><br>
+        <input type="number" id="dieselCost" name="dieselCost"><br>
+        <label for="petrolCost">Petrol Cost Per Trip:</label><br>
+        <input type="number" id="petrolCost" name="petrolCost"><br>
+        <input type="submit" value="submit">
+        <input type="reset" value="reset">
+    </form>
+
+ </body>
+</html>
